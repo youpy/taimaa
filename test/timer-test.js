c6c9788d75e911fb7e75014bd0baf1ad4ca965b6
youpy <youpy@buycheapviagraonlinenow.com>
2017-01-05 16:18:30 +0900

    add test for Timer#setInterval

--- test/timer-test.js
+++ test/timer-test.js
@@ -5,6 +5,10 @@ import assert from 'power-assert';
 describe('Timer', () => {
   const audioCtx = new AudioContext();
 
+  beforeEach(() => {
+    audioCtx.$reset();
+  });
+
   describe('#setTimeout', () => {
     it('calls given callback', () => {
       const t = new Timer(audioCtx);
@@ -39,6 +43,51 @@ describe('Timer', () => {
       audioCtx.$processTo('00:01.000');
 
       assert(!called);
+    });
+  });
+
+  describe('#setInterval', () => {
+    it('calls given callback', () => {
+      const t = new Timer(audioCtx);
+      var numCalls = 0;
+
+      t.setInterval(() => {
+        numCalls += 1;
+      }, 1000);
+
+      audioCtx.$processTo('00:00.500');
+
+      assert(numCalls === 0);
+
+      audioCtx.$processTo('00:01.000');
+
+      assert(numCalls === 1);
+
+      audioCtx.$processTo('00:02.000');
+
+      assert(numCalls === 2);
+    });
+
+    it('returns id for canceling', () => {
+      const t = new Timer(audioCtx);
+      var numCalls = 0;
+      const id = t.setInterval(() => {
+        numCalls++;
+      }, 1000);
+
+      audioCtx.$processTo('00:00.500');
+
+      assert(numCalls === 0);
+
+      audioCtx.$processTo('00:01.000');
+
+      assert(numCalls === 1);
+
+      t.clearTimeout(id);
+
+      audioCtx.$processTo('00:02.000');
+
+      assert(numCalls === 1);
     });
   });
 });
